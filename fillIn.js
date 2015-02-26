00157ae582c92b11cc6bbd7e078e728224939d97
Brian Capouch <brianc@saintjoe.edu>
2015-02-25 23:43:42 -0500

    A way to allow alternate answers

--- fillIn.js
+++ fillIn.js
@@ -1,12 +1,12 @@
 var FILLIN = [
-  {text: "Kennedy was first elected in *", value: "1960" },
-  {text: "* was the last state to join the union", value: "Alaska" },
-  {text: "Harry Truman was from * ", value: "Missouri" },
-  {text: "The Bay of Pigs was an invasion of * ", value: "Cuba"},
-  {text: "The first American in space was * ", value: "Alan Shepard" },
-  {text: "Apartheid ended in the South in * ", value: "1964" },
-  {text: "Lincoln's vice president was * ", value: "Andrew Johnson" },
-  {text: "* was the first state to join the union", value: "Delaware" }
+  {text: "Kennedy was first elected in *", values: ["1960"] },
+  {text: "* was the last state to join the union", values: ["Alaska"] },
+  {text: "Harry Truman was from * ", values: ["Missouri"] },
+  {text: "The Bay of Pigs was an invasion of * ", values: [ "Cuba" ]},
+  {text: "The first American in space was * ", values:[ "Alan Shepard", "Alan Shepherd", "Shepard", "Shepherd", "Allen Shepard", "Allen Shepherd" },
+  {text: "Apartheid ended in the South in * ", values: ["1964"]},
+  {text: "Lincoln's vice president was * ", values: ["Andrew Johnson", "Johnson"] },
+  {text: "* was the first state to join the union", values: ["Delaware"] }
 ];
 
 if (typeof module != "undefined" && module.exports)
