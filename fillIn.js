48d12cbd3f96f44baecaa6cb115598dd1e317fae
Brian Capouch <brianc@saintjoe.edu>
2015-02-25 23:29:00 -0500

    Initial Commit

---
+++ fillIn.js
@@ -0,0 +1,13 @@
+var FILLIN = [
+  {text: "Kennedy was first elected in *", value: "1960" },
+  {text: "* was the last state to join the union", value: "Alaska" },
+  {text: "Harry Truman was from * ", value: "Missouri" },
+  {text: "The Bay of Pigs was an invasion of * ", value: "Cuba"},
+  {text: "The first American in space was * ", value: "Alan Shepard" },
+  {text: "Apartheid ended in the South in * ", value: "1964" },
+  {text: "Lincoln's vice president was * ", value: "Andrew Johnson" },
+  {text: "* was the first state to join the union", value: "Delaware" }
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = FILLIN;
