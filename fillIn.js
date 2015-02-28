a39901c14f3aa9c0193e31059b5ea0d5094a9db7
Brian Capouch <brianc@saintjoe.edu>
2015-02-27 20:58:43 -0500

    Fix missing bracket

--- fillIn.js
+++ fillIn.js
@@ -3,7 +3,7 @@ var FILLIN = [
   {text: "* was the last state to join the union", values: ["Alaska"] },
   {text: "Harry Truman was from * ", values: ["Missouri"] },
   {text: "The Bay of Pigs was an invasion of * ", values: [ "Cuba" ]},
-  {text: "The first American in space was * ", values:[ "Alan Shepard", "Alan Shepherd", "Shepard", "Shepherd", "Allen Shepard", "Allen Shepherd" },
+  {text: "The first American in space was * ", values:[ "Alan Shepard", "Alan Shepherd", "Shepard", "Shepherd", "Allen Shepard", "Allen Shepherd" ]},
   {text: "Apartheid ended in the South in * ", values: ["1964"]},
   {text: "Lincoln's vice president was * ", values: ["Andrew Johnson", "Johnson"] },
   {text: "* was the first state to join the union", values: ["Delaware"] }
