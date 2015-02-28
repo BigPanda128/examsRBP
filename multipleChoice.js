51cf1562365cd721cde93b7ba6bfbfa18893dc92
Brian Capouch <brianc@saintjoe.edu>
2015-02-27 21:02:25 -0500

    Add odd decoy count numbers

--- multipleChoice.js
+++ multipleChoice.js
@@ -1,7 +1,7 @@
 var MULTIPLE = [
-  {text: "First state to legalize gay marriage", answer: "Massachusetts", decoys: ["California", "Oregon", "New York" ] },
+  {text: "First state to legalize gay marriage", answer: "Massachusetts", decoys: ["California", "Oregon", "New York", "Indiana", "Mississippi" ] },
   {text: "The last state to join the union", answer: "Hawaii", decoys: ["Alaska", "Arizona", "Kansas" ] },
-  {text: "Harry Truman\'s home state", answer: "Missouri", decoys: ["Illinois", "Kansas", "Indiana" ] },
+  {text: "Harry Truman\'s home state", answer: "Missouri", decoys: ["Illinois", "Kansas", "Indiana", "Ohio", "Kentucky" ] },
   {text: "Largest state by area", answer: "Alaska", decoys: ["Texas", "Montana", "Rhode Island" ] },
   {text: "John Glenn's home state", answer: "Ohio", decoys: ["Pennsylvania", "Arizona", "Florida" ] },
   {text: "State where Detroit Pistons first played", answer: "Indiana", decoys: ["Michigan", "Minnesota", "Ohio" ] },
