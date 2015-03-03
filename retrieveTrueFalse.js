0afa2eadc84afdcdbc9370160907dc144336a711
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-03-03 01:05:08 -0500

    a bit messy but had eureka moment. when i was inputing a true or false vlaue it was a string. so i needed a string to boolean method to do the test to see if they were true. in turn with doing that i got rid of a future problem that would revolve around case sensative answers.

--- retrieveTrueFalse.js
+++ retrieveTrueFalse.js
@@ -12,27 +12,51 @@ var rl = readline.createInterface({
 	  input: process.stdin,
       output: process.stdout
 });
-rl.question('line', function(line){
+
+stringToBoolean = function(string){
+	switch(string.toLowerCase()){
+		case "true": case "yes": case "1": return true;
+		case "false": case "no": case "0": case null: return false;
+		default: return Boolean(string);
+	}
+}
+
+/*rl.question('line', function(line){
 	    console.log(line);
 	    rl.close();
-})
+})*/
 
 //console.log(data[0].text);
 
 var text = [];
 
 var answers = [];
-
+/*
 for (var i = 0; i < data.length; ++i) {
-	console.log(data[i].text);
-	console.log("true or false? \n");
-	var input = readline();
-	console.log(data[i].value);
-	if (input == data[i].value) {
+	console.log(data[i].text + "\n");
+}*/
+
+console.log('Enter true or false \n');
+
+rl.question(data[0].text + " ", function(line){
+	console.log("You answered: ", line);
+	console.log(typeof line);
+	if (stringToBoolean(line) === data[0].value) {
 		answers.push("correct");
 	}
 	else {
 		answers.push("incorrect");
 	}
-}
+
+	rl.close();
+	console.log(answers);
+});
+	//var input = readline();
+//console.log(data[i].value);
+	/*if (input == data[i].value) {
+		answers.push("correct");
+	}
+	else {
+		answers.push("incorrect");
+	}*/
 
